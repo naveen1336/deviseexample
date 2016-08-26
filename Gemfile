b4e51a53fe7274242b37efda16e65899cecd023d
Naveen <nk1383886@gmail.com>
2016-08-26 12:42:24 +0530

    added github omniauth rails 5 with devise

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'therubyracer'
 gem 'devise'
 gem 'omniauth'
-gem 'omniauth-digitalocean'
+gem 'omniauth-github'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+config.omniauth :github, 'd8c788ae556c6767fef2', 'eb4e7c6dba89aee9312ab537a331ca72ec0ea633', :scope => 'user:email'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
